c5cc82fba2525f1fdaceeb760f1c1444ee1fb458
Lance <lancehahn@gmail.com>
2022-09-29 00:19:30 +0900

    add the deploy setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.17", 
+  defaultNetwork: "ropsten",
+  networks: {
+     hardhat: {},
+     ropsten: {
+        url: process.env.API_URL,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+     }
+  },
 };
